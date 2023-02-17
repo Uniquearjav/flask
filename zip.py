e2637fc6c0f1c783ec7dd87b615acbd319c7fd9f
Arjav Choudhary <92720814+Uniquearjav@users.noreply.github.com>
2023-02-17 20:26:39 +0530

    Delete zip.py

--- zip.py
+++
@@ -1,9 +0,0 @@
-import zipfile
-
-# Name of the zip file you want to extract
-zip_file = 'horizaura-master.zip'
-
-# Open the zip file in read mode
-with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-    # Extract all files to the current directory
-    zip_ref.extractall('.')
