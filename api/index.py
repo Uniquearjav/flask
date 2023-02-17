26a4b1d8a33561a8b590889fde55fef0887b0108
Arjav Choudhary <92720814+Uniquearjav@users.noreply.github.com>
2023-02-17 21:02:25 +0530

    header

--- api/index.py
+++ api/index.py
@@ -1,10 +1,10 @@
 from flask import Flask , render_template
 
 app = Flask(__name__)
-
-@app.route("/")
 def home():
-    return render_template("index.html")
+    with open('header.html') as f:
+        header_content = f.read()
+    return render_template('index.html', header_content=header_content)
 
 @app.route("/login")
 def login():
